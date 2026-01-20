09fea58e4e79a37d92f5a567d41acd3bf9802224
mdbayzid2025 <ssmdbayzid23@gmail.com>
2026-01-20 14:36:13 +0600

    update

--- src/redux/base/baseAPI.ts
+++ src/redux/base/baseAPI.ts
@@ -6,8 +6,8 @@ export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
  
-    baseUrl: "http://10.10.7.48:5000/api/v1",    
-    // baseUrl: "https://rakibur5000.binarybards.online/api/v1",    
+    // baseUrl: "http://10.10.7.48:5000/api/v1",    
+    baseUrl: "https://rakibur5000.binarybards.online/api/v1",    
     prepareHeaders: (headers) => {
       // headers.set("ngrok-skip-browser-warning", "true");
       const token = Cookies.get("accessToken");
@@ -22,5 +22,5 @@ export const baseApi = createApi({
   tagTypes: ["user", "notifications", "admin", "category", "slider", "faqs", "withdrawal", "planner"],
 });
 
-export const imageUrl = "http://10.10.7.48:5000";
-// export const imageUrl = "https://rakibur5000.binarybards.online";
+// export const imageUrl = "http://10.10.7.48:5000";
+export const imageUrl = "https://rakibur5000.binarybards.online";
